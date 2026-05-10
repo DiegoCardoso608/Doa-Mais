2cda0fdc69a94e3892c97a94baf72958403d5695
Diego Cardoso <diegocardosodasilva608@gmail.com>
2026-05-09 21:04:37 -0300

    Integração Firebase e configuração FlutterFire no Doa+

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
 import 'screens/main_screen.dart';
 import 'screens/login_pf.dart';
 import 'screens/empresa_login.dart';
 import 'screens/cadastro_pf.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const DoaPlusApp());
 }
 
@@ -21,9 +31,7 @@ class DoaPlusApp extends StatelessWidget {
         '/': (context) => const MainScreen(),
         '/login_pf': (context) => const LoginPF(),
         '/empresa_login': (context) => const EmpresaLogin(),
-
         '/cadastro_pf': (context) => const CadastroPF(),
-        
       },
     );
   }
